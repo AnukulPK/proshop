e43e3a0a5ede0f609dace7080f3c87f774370f8a
Anukul <anukul.p@growthsourceft.com>
2021-08-19 14:39:44 +0530

    added nodemon and concurrently

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -23,7 +23,7 @@ const ProductScreen = ({ match }) => {
     };
 
     fetchProduct();
-  }, [match.params.id]);
+  }, [match]);
 
   // const product = products.find((product) => product._id === match.params.id);
 
