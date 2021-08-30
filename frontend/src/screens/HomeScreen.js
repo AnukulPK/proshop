1a94a5ae8259ac3c39d71af9a74a96c552b7eb2a
Anukul <anukul.p@growthsourceft.com>
2021-08-30 15:43:10 +0530

    Loader and Error message component added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product/Product';
 import { Row, Col } from 'react-bootstrap';
 import { listProducts } from '../redux/actions/productActions';
+import Loader from '../components/Loader/Loader';
+import Message from '../components/Message/Message';
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
@@ -17,9 +19,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
